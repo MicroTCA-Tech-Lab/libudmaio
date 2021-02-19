e16af5e205b5033a2851d00926e9097469f7c7e3
Jan Marjanovic <jan.marjanovic@desy.de>
2021-02-19 17:01:03 +0100

    dev-irq: add mode sel - XDMA or ARM (u-dma-buf)

--- src/axi_dma_demo.cpp
+++ src/axi_dma_demo.cpp
@@ -20,6 +20,7 @@
 #include <boost/log/trivial.hpp>
 #include <boost/program_options.hpp>
 
+#include "FpgaDdr4Buffer.hpp"
 #include "UDmaBuf.hpp"
 #include "UioAxiDmaIf.hpp"
 #include "UioIf.hpp"
@@ -32,21 +33,37 @@
 namespace blt = boost::log::trivial;
 namespace bpo = boost::program_options;
 
+enum DmaMode { XDMA, ARM };
+
+std::istream &operator>>(std::istream &in, DmaMode &mode) {
+    std::string token;
+    in >> token;
+    if (token == "xdma")
+        mode = DmaMode::XDMA;
+    else if (token == "arm")
+        mode = DmaMode::ARM;
+    else
+        in.setstate(std::ios_base::failbit);
+    return in;
+}
+
 int main(int argc, char *argv[]) {
     bpo::options_description desc("AXI DMA demo");
     bool debug, trace;
     uint16_t pkt_pause;
     uint16_t nr_pkts;
     uint32_t pkt_len;
+    DmaMode mode;
 
     // clang-format off
     desc.add_options()
-	("help,h", "this help")
-	("debug", bpo::bool_switch(&debug), "enable verbose output (debug level)")
-	("trace", bpo::bool_switch(&trace), "enable even more verbose output (trace level)")
-	("pkt_pause", bpo::value<uint16_t>(&pkt_pause)->default_value(10), "pause between pkts - see AXI TG user's manual")
-	("nr_pkts", bpo::value<uint16_t>(&nr_pkts)->default_value(1), "number of packets to generate - see AXI TG user's manual")
-	("pkt_len", bpo::value<uint32_t>(&pkt_len)->default_value(1024), "packet length - see AXI TG user's manual")
+    ("help,h", "this help")
+    ("mode", bpo::value<DmaMode>(&mode)->multitoken()->required(), "select operation mode - see docs for details")
+    ("debug", bpo::bool_switch(&debug), "enable verbose output (debug level)")
+    ("trace", bpo::bool_switch(&trace), "enable even more verbose output (trace level)")
+    ("pkt_pause", bpo::value<uint16_t>(&pkt_pause)->default_value(10), "pause between pkts - see AXI TG user's manual")
+    ("nr_pkts", bpo::value<uint16_t>(&nr_pkts)->default_value(1), "number of packets to generate - see AXI TG user's manual")
+    ("pkt_len", bpo::value<uint32_t>(&pkt_len)->default_value(1024), "packet length - see AXI TG user's manual")
     ;
     // clang-format on
 
@@ -67,17 +84,25 @@ int main(int argc, char *argv[]) {
         boost::log::core::get()->set_filter(blt::severity >= blt::info);
     }
 
-    auto axi_dma = UioIfFactory::create<UioAxiDmaIf>("axi_dma_0");
-    auto mem_sgdma = UioIfFactory::create<UioMemSgdma>("axi_bram_ctrl_0");
-    auto traffic_gen = UioIfFactory::create<UioTrafficGen>("axi_traffic_gen_0");
-    UDmaBuf udmabuf;
+    auto axi_dma = (mode == DmaMode::ARM) ? UioIfFactory::create<UioAxiDmaIf>("axi_dma_0")
+                                          : UioIfFactory::create_from_xdma<UioAxiDmaIf>(
+                                                0x00910000, 4 * 1024, "/dev/xdma/card0/events0");
+    auto mem_sgdma = (mode == DmaMode::ARM)
+                         ? UioIfFactory::create<UioMemSgdma>("axi_bram_ctrl_0")
+                         : UioIfFactory::create_from_xdma<UioMemSgdma>(0x00920000, 8 * 1024);
+    auto traffic_gen = (mode == DmaMode::ARM)
+                           ? UioIfFactory::create<UioTrafficGen>("axi_traffic_gen_0")
+                           : UioIfFactory::create_from_xdma<UioTrafficGen>(0x00890000, 64 * 1024);
+    DmaBufferAbstract *udmabuf = (mode == DmaMode::ARM)
+                                     ? static_cast<DmaBufferAbstract *>(new UDmaBuf{})
+                                     : static_cast<DmaBufferAbstract *>(new FpgaDdr4Buffer{});
     uint64_t counter_ok = 0, counter_total = 0;
-    DataHandlerPrint data_handler{*axi_dma, *mem_sgdma, udmabuf, counter_ok, counter_total};
+    DataHandlerPrint data_handler{*axi_dma, *mem_sgdma, *udmabuf, counter_ok, counter_total};
     std::thread t1{data_handler};
 
     std::vector<uint64_t> dst_buf_addrs;
     for (int i = 0; i < 32; i++) {
-        dst_buf_addrs.push_back(udmabuf.get_phys_addr() + i * mem_sgdma->BUF_LEN);
+        dst_buf_addrs.push_back(udmabuf->get_phys_addr() + i * mem_sgdma->BUF_LEN);
     };
 
     mem_sgdma->write_cyc_mode(dst_buf_addrs);
