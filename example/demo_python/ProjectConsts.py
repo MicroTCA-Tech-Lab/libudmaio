f213c2c89bee2573838986da52f25109e1a9fdf8
Patrick Huesmann <patrick.huesmann@desy.de>
2023-01-18 18:24:53 +0100

    demo_python: Fix addresses for ZUP example design

--- example/demo_python/ProjectConsts.py
+++ example/demo_python/ProjectConsts.py
@@ -5,20 +5,20 @@ from pyudmaio import UioDeviceLocation, UioRegion
 
 class ZupExampleConsts(object):
     AXI_GPIO_STATUS = UioDeviceLocation(
-        'axi_gpio_status', UioRegion(0x00801000, 4 * 1024)
+        'hier_interconnect_axi_gpio_status', UioRegion(0x50000, 4 * 1024)
     )
     AXI_DMA_0 = UioDeviceLocation(
-        'hier_daq_arm_axi_dma_0', UioRegion(0x00910000, 4 * 1024), 'events0'
+        'hier_daq_arm_axi_dma', UioRegion(0x222000, 4 * 1024), 'events0'
     )
     BRAM_CTRL_0 = UioDeviceLocation(
-        'hier_daq_arm_axi_bram_ctrl_0', UioRegion(0x00920000, 8 * 1024)
+        'hier_daq_arm_axi_bram_ctrl', UioRegion(0x220000, 8 * 1024)
     )
     AXI_TRAFFIC_GEN_0 = UioDeviceLocation(
-        'hier_daq_arm_axi_traffic_gen_0', UioRegion(0x00890000, 64 * 1024)
+        'hier_daq_arm_axi_traffic_gen', UioRegion(0x210000, 64 * 1024)
     )
 
     FPGA_MEM_PHYS_ADDR = 0x500000000
-    PCIE_AXI4L_OFFSET = 0x88000000
+    PCIE_AXI4L_OFFSET = 0xa0000000
     LFSR_BYTES_PER_BEAT = 16
 
 
